0c58a7b62be3de21d90be3138c34774e5d4e580b
Cole Winslow <colethomaswinslow7@gmail.com>
2022-02-25 08:33:55 -0700

    Update

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import "./App.css";
 import Contact from "./components/Contact/Contact";
-import Footer from "./components/Footer/Footer";
+
 import Hero from "./components/Hero/Hero";
 import NavBar from "./components/Navbar/NavBar";
 import Projects from "./components/Projects/Projects";

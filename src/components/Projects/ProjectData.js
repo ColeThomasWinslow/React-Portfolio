64e582dc57b61de62a768b6690acecb2bf6f9f37
Cole Winslow <colethomaswinslow7@gmail.com>
2022-03-08 16:48:53 -0700

    Added Goal Setter

--- src/components/Projects/ProjectData.js
+++ src/components/Projects/ProjectData.js
@@ -1,7 +1,7 @@
 import Sorting from "./ProjectPhotos/Sorting.png";
 import FakeShop from "./ProjectPhotos/FakeShop.png";
 import CarShowSocial from "./ProjectPhotos/CarShowSocial.png";
-import StatesAndCapitals from "./ProjectPhotos/StatesAndCapitals.png";
+import Goal from "./ProjectPhotos/Goal.svg";
 import TypeMaster from "./ProjectPhotos/TypeMaster.png";
 import BudgetTracker from "./ProjectPhotos/BudgetTracker.png";
 
@@ -13,6 +13,14 @@ export const projectData = [
     desc: "Visualization tool to compare different sorting algorithms and see the time complexity for each method.",
     gitLink: "https://github.com/ColeThomasWinslow/sorting-algo-visualizer",
     LiveLink: "https://sortalgorithms.netlify.app/",
+  },
+  {
+    id: 2,
+    title: "Goal Setter",
+    img: Goal,
+    desc: " Web application to track goals. Create goals with different priority levels to manage which goals to start with first.  ",
+    gitLink: "https://github.com/ColeThomasWinslow/Goal-Tracker",
+    LiveLink: "https://goalsettercw.herokuapp.com/",
   },
   {
     id: 2,
@@ -30,14 +38,7 @@ export const projectData = [
     gitLink: "https://github.com/ColeThomasWinslow/Car-Show-Social",
     LiveLink: "https://carshowsocial.netlify.app/",
   },
-  {
-    id: 4,
-    title: "States And Capitals API",
-    img: StatesAndCapitals,
-    desc: " Developer Api to retrieve data related to the United States of America. Get data for states, capitals, population sizes, and Senators",
-    gitLink: "https://github.com/ColeThomasWinslow/States-and-Capitals",
-    LiveLink: "https://statescapitals.netlify.app/",
-  },
+
   {
     id: 5,
     title: "TypeMaster",
@@ -45,13 +46,5 @@ export const projectData = [
     desc: "Web application designed to help users type faster, with a timed typing test to see how many WPM can be typed.",
     gitLink: "https://github.com/ColeThomasWinslow/type-master",
     LiveLink: "https://typemasterctw.netlify.app/",
-  },
-  {
-    id: 6,
-    title: "Budget Tracker",
-    img: BudgetTracker,
-    desc: "Budgeting application that tracks income and expenses to reflect the users current balance and stores all of the transactions placed.",
-    gitLink: "https://github.com/ColeThomasWinslow/Budget-Tracker",
-    LiveLink: "https://fierce-sierra-84574.herokuapp.com/",
   },
 ];

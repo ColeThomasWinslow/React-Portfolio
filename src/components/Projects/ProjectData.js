d6786f19afe061c3c62f5fbeddb35adf2ba4dd45
Cole Winslow <colethomaswinslow7@gmail.com>
2022-03-24 19:20:39 -0700

    Fixed Projects

--- src/components/Projects/ProjectData.js
+++ src/components/Projects/ProjectData.js
@@ -1,23 +1,7 @@
 import Sorting from "./ProjectPhotos/Sorting.png";
 import PayRoll from "./ProjectPhotos/PayRoll.png";
-import FitnessPortal from "./ProjectPhotos/FitnessPortal.png";
-import CoderComments from "./ProjectPhotos/CoderComments.png";
 import CalculationFinance from "./ProjectPhotos/CalculationFinance.png";
 export const projectData = [
-  {
-    id: 1,
-    title: "Sorting Visualizer ",
-    img: Sorting,
-    tech: [
-      { name: "Vanilla Javascript" },
-      { name: "CSS Animations" },
-      { name: " Data Structures" },
-      { name: "Algorithms" },
-    ],
-    desc: "Visualization tool to compare different sorting algorithms and see the time complexity for each method.",
-    gitLink: "https://github.com/ColeThomasWinslow/sorting-algo-visualizer",
-    LiveLink: "https://sortalgorithms.netlify.app/",
-  },
   {
     id: 1,
     title: "Payroll",
@@ -35,7 +19,7 @@ export const projectData = [
     LiveLink: "https://payroll-app-ctw.herokuapp.com/Login",
   },
   {
-    id: 1,
+    id: 2,
     title: "Calculation Finance",
     img: CalculationFinance,
     tech: [
@@ -49,25 +33,16 @@ export const projectData = [
     LiveLink: "https://calculationfinance.netlify.app/",
   },
   {
-    id: 1,
-    title: "Coder Comments | web development blog",
-    img: CoderComments,
-    tech: [{ name: "Gatsby" }, { name: "Tailwind CSS" }],
-    desc: "Web application to track salary and payroll within a company, in addition logging employee contact information.",
-    gitLink: "https://github.com/ColeThomasWinslow/sorting-algo-visualizer",
-    LiveLink: "https://sortalgorithms.netlify.app/",
-  },
-  {
-    id: 1,
-    title: "Fitness Portal",
-    img: FitnessPortal,
+    id: 3,
+    title: "Sorting Visualizer ",
+    img: Sorting,
     tech: [
-      { name: "React.js" },
-      { name: "Tailwind CSS" },
-      { name: "Mongo DB" },
-      { name: "Node & Express" },
+      { name: "Vanilla Javascript" },
+      { name: "CSS Animations" },
+      { name: " Data Structures" },
+      { name: "Algorithms" },
     ],
-    desc: "Fitness software developed to track your workouts as well as your diet ",
+    desc: "Visualization tool to compare different sorting algorithms and see the time complexity for each method.",
     gitLink: "https://github.com/ColeThomasWinslow/sorting-algo-visualizer",
     LiveLink: "https://sortalgorithms.netlify.app/",
   },

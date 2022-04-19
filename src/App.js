e306af78d31842ecdd7e775e3e6e6319673f5a42
Cole Winslow <colethomaswinslow7@gmail.com>
2022-04-19 16:52:07 -0700

    Added Resume Tooltip

--- src/App.js
+++ src/App.js
@@ -1,9 +1,7 @@
 import "./App.css";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-
 import Home from "./Pages/Home";
 import Conformation from "./Pages/Conformation";
-import Resume from "./Pages/Resume";
 
 function App() {
   return (
@@ -11,7 +9,7 @@ function App() {
       <div className="App">
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/resume" element={<Resume />} />
+
           <Route path="/confirm" element={<Conformation />} />
         </Routes>
       </div>
--- src/components/Navbar/ExternalLinks.js
+++ src/components/Navbar/ExternalLinks.js
@@ -1,8 +1,9 @@
 import React from "react";
 import Github from "../../icons/Github.svg";
 import Linkedin from "../../icons/Linkedin.svg";
-
-import { Link } from "react-router-dom";
+import ResumeFILE from "../../icons/ColeWinslowResume.pdf";
+import Download from "../../icons/download.svg";
+import Open from "../../icons/Open.svg";
 
 function ExternalLinks() {
   return (
@@ -21,9 +22,23 @@ function ExternalLinks() {
       >
         <img className="Icon" src={Linkedin} alt="Linkedin" />
       </a>
-      <Link to="resume" className="Btn SmallBtn">
+      <div to="resume" className="Btn SmallBtn tooltip ">
         Resume
-      </Link>
+        <div className="tooltiptext">
+          <a href={ResumeFILE} download="ColeWinslowResume" className="Tag">
+            <p>Download Resume</p> <img src={Download} alt="Download" />
+          </a>
+          <a
+            rel="noreferrer"
+            target="_blank"
+            type="application/pdf"
+            href={ResumeFILE}
+            className="Tag "
+          >
+            <p>Open Resume</p> <img src={Open} alt="Open" />
+          </a>
+        </div>
+      </div>
     </div>
   );
 }
